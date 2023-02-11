0ff47085bfa68369bab4658dbd61c76e402bec50
igor dos santos <igsanbody@gmail.com>
2023-02-11 13:17:50 -0300

    Primeiro commit

---
+++ src/main/java/teste/Primeiro_programa.java
@@ -0,0 +1,26 @@
+package teste;
+
+public class Primeiro_programa {
+
+	
+	private String nome;
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world");
+	}
+
+	public Primeiro_programa(String nome) {
+		super();
+		this.nome = nome;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+}
